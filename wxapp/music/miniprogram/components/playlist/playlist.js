2daf3b22d9fd617faf6e74d713381fe5984c643f
anpeier <1490343356@qq.com>
2019-12-07 10:10:28 +0800

    音乐

---
+++ wxapp/music/miniprogram/components/playlist/playlist.js
@@ -0,0 +1,47 @@
+// components/playlist/playlist.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    playlist: {
+      type: Object,
+    }
+  },
+
+  observers: {
+    ['playlist.playCount'](count) {
+      // console.log(val)
+      console.log(this._tranNumber(count,2))
+      this.setData({
+        _count: this._tranNumber(count,2)
+      })
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    _count: 0
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    _tranNumber(num, point) {
+      let numStr = num.toString().split('.')[0]
+      if (numStr.length < 6) {
+        return numStr
+      } else if (numStr.length >= 6 && numStr.length <= 8) {
+        let decimal = numStr.substring(numStr.length - 4, numStr.length - 4 + point)
+        return parseFloat(parseInt(num / 10000) + '.' + decimal) +
+          '万'
+      } else if (numStr.length > 8) {
+        let decimal = numStr.substring(numStr.length - 8, numStr.length - 8 + point)
+        return parseFloat(parseInt(num / 100000000) + '.' + decimal) + '亿'
+      }
+    }
+  }
+})
