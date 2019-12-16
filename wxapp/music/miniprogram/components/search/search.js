403548faa10111b235aeb17630158c78b85533e2
anpeier <1490343356@qq.com>
2019-12-16 20:38:08 +0800

    editInPlace

---
+++ wxapp/music/miniprogram/components/search/search.js
@@ -0,0 +1,31 @@
+// components/search/search.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    placeholder: {
+      type: String,
+      value: '请输入关键字'
+    }
+  },
+
+  externalClasses: [
+    'iconfont',
+    'icon-soushuo',
+  ],
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
