3d69e1bf6e0ccc73852ba83e592a90e0ac7eaf36
anpeier <1490343356@qq.com>
2019-12-28 17:29:08 +0800

    vue

---
+++ vue/waimai/README.md
@@ -0,0 +1,7 @@
+- 组件式开发
+  页面由组件构成(拼乐高)，而非标签(传统切页面)
+  Facebook 由一万多个组件拼起来，组件的复用
+  components/header/header 效力于多个页面
+- Object.assign({}, obj, obj, ...)
+  来自es6，用于对象的合并，将源对象（source）的所有可枚举属性，复制到目标对象（target）
+- import from 模块化开发
---
+++ vue/waimai/src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/goods',
+    name: 'Goods',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Goods.vue')
+  },
+  {
+    path: '/ratings',
+    name: 'Ratings',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Ratings.vue')
+  },
+  {
+    path: '/seller',
+    name: 'Seller',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Seller.vue')
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
