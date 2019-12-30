d83c750068377250a6c6eb5fa4fa6b25ffa08343
anpeier <1490343356@qq.com>
2019-12-30 21:41:14 +0800

    vue

---
+++ vue/163music/src/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="app">
+    <div class="user">
+      <div class="back" @click="back">
+        <i class="fa fa-angle-left"></i>
+      </div>
+      <div class="switches-wrapper">
+      <switches :switches="switches" 
+      :currentIndex="currentIndex"
+       @switch="changeItem"></switches>
+    </div>
+    </div>
+  </div>
+</template>
+<script>
+import Switches from './components/switches/switches'
+export default {
+  methods: {
+    back() {
+    console.log('返回')
+    },
+    changeItem(index){
+      this.currentIndex = index;
+    }
+  },
+  components: {
+    'switches': Switches
+  },
+  data() {
+    //这里存放数据
+    return {
+      currentIndex: 0,
+      switches: [
+        {
+          name: '我的收藏'
+        },
+        {
+          name: '最近播放'
+        }
+      ]
+    };
+  },
+}
+</script>
+<style lang="stylus">
+@import "./common/styl/reset";
+@import "./common/styl/variable";
+
+@font-face {
+  font-family: 'FontAwesome';
+  src: url('./common/font-awesome/fonts/fontawesome-webfont.eot');
+  src: url('./common/font-awesome/fonts/fontawesome-webfont.eot?#iefix') format('embedded-opentype'),
+    url('./common/font-awesome/fonts/fontawesome-webfont.woff2') format('woff2'),
+    url('./common/font-awesome/fonts/fontawesome-webfont.woff') format('woff'),
+    url('./common/font-awesome/fonts/fontawesome-webfont.ttf') format('truetype'),
+    url('./common/font-awesome/fonts/fontawesome-webfont.svg') format('svg');
+  font-weight: normal;
+  font-style: normal;
+}
+
+.fa
+  display inline-block
+  font normal normal normal 14px/1 FontAwesome
+  font-size inherit
+  text-rendering auto
+  -webkit-font-smoothing antialiased
+
+.user
+  position fixed
+  top 0
+  bottom 0
+  left 0
+  right 0
+  z-index 100
+  background $color-background
+  .back
+    position absolute
+    top 0
+    left 4px
+    .fa-angle-left
+      padding 5px 10px
+      font-size 30px
+      color #aaa
+      &:before
+        content "\F104"
+  .switches-wrapper
+    display flex 
+    align-items center
+    background red
+    height 44px
+</style>
---
+++ vue/163music/src/components/switches/switches.vue
@@ -0,0 +1,65 @@
+<!--  -->
+<template>
+    <ul class="switches">
+        <li v-for="(item, index) in switches" 
+        :key="item.id" 
+        class="switches-item"
+        :class="{'active' : currentIndex === index}"
+        @click="changeItem(index)">
+            <span>{{item.name}}</span>
+        </li>
+    </ul>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+
+export default {
+    //外来数据
+    props: {
+        switches: {
+            type: Array,
+            required: true
+        },
+        currentIndex: {
+            type: Number,
+            defalut: 0
+        }
+    },
+    //import引入的组件需要注入到对象中才能使用
+    components: {},
+    data() {
+    //这里存放自有数据
+    return {
+
+        };
+    },
+    methods: {
+      changeItem(index){
+        // 组件通信
+        this.$emit('switch', index)   
+      }
+    }
+}
+</script>
+<style lang='stylus'>
+    //@import url(); 引入公共css类
+    @import "../../common/styl/variable";
+    .switches
+      display flex 
+      align-items center
+      width 240px
+      margin 0 auto
+      .switches-item
+        flex 1
+        padding 8px
+        margin 0 20px
+        text-align center
+        font-size $font-size-medium-x
+        color $color-text-l
+        border-bottom 2px solid $color-theme
+        &.active
+          color #fff
+          border-bottom 2px solid $color-text-l
+</style>
