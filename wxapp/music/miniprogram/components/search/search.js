3c39fb2cf84f6f61834e69218269bea52f8ad284
anpeier <1490343356@qq.com>
2020-01-22 16:30:56 +0800

    音乐小程序

--- wxapp/music/miniprogram/components/search/search.js
+++ wxapp/music/miniprogram/components/search/search.js
@@ -1,4 +1,5 @@
 // components/search/search.js
+let keyword = ''
 Component({
   /**
    * 组件的属性列表
@@ -27,6 +28,11 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onInput(event) {
+      keyword = event.detail.value
+    },
+    onSearch() {
+      console.log(keyword)
+    },
   }
 })
