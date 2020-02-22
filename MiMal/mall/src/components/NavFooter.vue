902dc1d9592656140b26160554d84c8f8550fb65
anpeier <1490343356@qq.com>
2020-02-22 13:23:45 +0800

    FooterBar

--- MiMal/mall/src/components/NavFooter.vue
+++ MiMal/mall/src/components/NavFooter.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="/imgs/logo-footer.png" alt="">
+      <p href="">小米商城</p>
+    </div>
+    <div class="footer-link">
+      <a href="https://anpeier.github.io/" target="_blank">安培儿主页</a><span>|</span>
+      <a href="#">Vue学习</a><span>|</span>
+      <a href="#">微信小程序学习</a><span>|</span>
+      <a href="#">Node学习</a><span>|</span>
+      <a href="#">Java学习</a>
+    </div>
+    <div class="copyright">Copyright @2019 <span class="domain">anpeier.github.io</span>欢迎访问</div>
   </div>
 </template>
 
@@ -9,6 +21,37 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+.footer{
+  height: 234px;
+  border-top: 4px solid #FF6600;
+  background-color: #333333;
+  color: #999999;
+  font-size: 16px;
+  text-align: center;
+  .footer-logo{
+    margin-top: 46px;
+    margin-bottom: 31px;
+    img{
+      width: 53px;
+      height: 36px;
+      margin-bottom: 13px;
+    }
+  }
+  .footer-link{
+    a{
+      color: #999999;
+      display: inline-block;
+    }
+    span{
+      margin: 0 10px
+    }
+    margin-bottom: 10px;
+  }
+  .copyright{
+    .domain{
+      color: #FF6600;
+    }
+  }
+}
 </style>
