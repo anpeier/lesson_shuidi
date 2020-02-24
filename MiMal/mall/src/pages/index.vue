9c4dd5db6177cf84933889ffae4674ac160cac49
anpeier <1490343356@qq.com>
2020-02-25 01:46:14 +0800

    首页轮播菜单

--- MiMal/mall/src/pages/index.vue
+++ MiMal/mall/src/pages/index.vue
@@ -6,42 +6,44 @@
           <ul class="menu-wrap">
             <li class="menu-item">
               <a href="javascript:;">手机 电话卡</a>
-              <div class="children"></div>
+              <div class="children">
+                <ul v-for="(item, idx) in menuList" :key="idx">
+                  <li v-for="sub in item" :key="sub.id">
+                    <a :href="sub ? '/#/product/' + sub.id : ''">
+                      <img :src="sub ? sub.img : '/imgs/item-box-1.png'" alt />
+                      {{ sub ? sub.name : "小米9" }}
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </li>
             <li class="menu-item">
               <a href="javascript:;">电视 盒子</a>
-              <div class="children"></div>
             </li>
             <li class="menu-item">
               <a href="javascript:;">笔记本 平板</a>
-              <div class="children"></div>
             </li>
             <li class="menu-item">
               <a href="javascript:;">家电 插线板</a>
-              <div class="children"></div>
             </li>
             <li class="menu-item">
               <a href="javascript:;">出行 穿戴</a>
-              <div class="children"></div>
             </li>
             <li class="menu-item">
               <a href="javascript:;">智能 路由器</a>
-              <div class="children"></div>
             </li>
             <li class="menu-item">
               <a href="javascript:;">电源 配件</a>
-              <div class="children"></div>
             </li>
             <li class="menu-item">
               <a href="javascript:;">生活 箱包</a>
-              <div class="children"></div>
             </li>
           </ul>
         </div>
         <swiper :options="swiperOption">
           <swiper-slide v-for="(item, idx) in slideList" :key="idx">
-            <a :href="'/#/product/' + item.id">
-              <img :src="item.img" alt />
+            <a v-bind:href="'/#/product/' + item.id">
+              <img v-bind:src="item.img" alt />
             </a>
           </swiper-slide>
           <div class="swiper-pagination" slot="pagination"></div>
@@ -110,6 +112,35 @@ export default {
           id: "",
           img: "/imgs/slider/slide-5.jpg"
         }
+      ],
+      menuList: [
+        [
+          {
+            id: 30,
+            img: "/imgs/item-box-1.png",
+            name: "小米CC9"
+          },
+          {
+            id: 31,
+            img: "/imgs/item-box-2.png",
+            name: "小米8青春版"
+          },
+          {
+            id: 32,
+            img: "/imgs/item-box-3.jpg",
+            name: "Redmi K20 Pro"
+          },
+          {
+            id: 33,
+            img: "/imgs/item-box-4.jpg",
+            name: "移动4G专区"
+          }
+        ],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
       ]
     };
   }
@@ -128,7 +159,7 @@ export default {
       height: 451px;
       z-index: 9;
       padding: 26px 0;
-      background-color: #55585A7A; // 透明度7A
+      background-color: #55585a7a; // 透明度7A
 
       .menu-wrap {
         .menu-item {
@@ -144,19 +175,53 @@ export default {
               position: absolute;
               top: 17.5px;
               right: 30px;
-              content: '';
-              @include bgImg(10px, 15px, '/imgs/icon-arrow.png')
+              content: "";
+              @include bgImg(10px, 15px, "/imgs/icon-arrow.png");
             }
           }
-          &:hover{
+          &:hover {
             background-color: $colorA;
+            .children{
+              display: block;
+            }
+          }
+          .children {
+            display: none;
+            width: 962px;
+            height: 451px;
+            background-color: $colorG;
+            position: absolute;
+            top: 0;
+            left: 264px;
+            border: 1px solid $colorH;
+            ul {
+              display: flex;
+              justify-content: space-between;
+              height: 75px;
+              li {
+                height: 75px;
+                line-height: 75px;
+                flex: 1;
+                padding-left: 23px;
+              }
+              a {
+                color: $colorB;
+                font-size: 14px;
+              }
+              img {
+                width: 42px;
+                height: 35px;
+                vertical-align: middle;
+                margin-right: 10px;
+              }
+            }
           }
         }
       }
     }
     .swiper-container {
       height: 451px;
-      .swiper-button-prev{
+      .swiper-button-prev {
         left: 274px;
       }
       img {
