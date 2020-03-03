68f34a6e2a2ca36b7ffb88e6e8456a9a89ac5342
anpeier <1490343356@qq.com>
2020-03-03 19:48:23 +0800

    上线流程

---
+++ vue/ele_unit/admin/vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // devServer: {
+  //   // '127.0.0.1:8080/api/admin/login'
+  //   proxy: {
+  //     '/api': {
+  //       //转发到 伪装成3000
+  //       target: 'http://127.0.0.1:3000',
+  //       changeOrigin: true,
+  //       // http://127.0.0.1:3000/admin/login
+  //       pathRewrite: {
+  //         'api': ''
+  //       }
+  //     }
+  //   }
+  // },
+  publicPath: '/admin'
+}
---
+++ vue/ele_unit/ele-node/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+// 路由的模块化
+// api
+const router = require('./routes/index.js');
+// web  express.static 启用静态服务器
+app.use('/', express.static(path.join(__dirname, 'web')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/admin', express.static(path.join(__dirname, 'admin')));
+router(app);
+
+// app.use()
+app.listen("3000", () => {
+  console.log('api 服务器上线了');
+})
