ebc973473cfb10c764a182d5f89c6e56031fb7f3
anpeier <1490343356@qq.com>
2020-03-21 20:44:19 +0800

    水壶问题

---
+++ leetcode/canMeasureWater/1.js
@@ -0,0 +1,24 @@
+// 365.水壶问题
+// 有两个容量分别为 x升 和 y升 的水壶以及无限多的水。请判断能否通过使用这两个水壶，
+// 从而可以得到恰好 z升 的水？
+// 如果可以，最后请用以上水壶中的一或两个来盛放取得的 z升 水。
+// 你允许：
+// 装满任意一个水壶
+// 清空任意一个水壶
+// 从一个水壶向另外一个水壶倒水，直到装满或者倒空
+// 示例 1: (From the famous "Die Hard" example)
+// 输入: x = 3, y = 5, z = 4
+// 输出: True
+// 示例 2:
+// 输入: x = 2, y = 6, z = 5
+// 输出: False
+
+var canMeasureWater = function(x, y, z) {
+  if(z == 0)return true
+  if(x + y < z) return false
+  if(z % Math.abs(x%y-y) == 0) return true
+  else return false
+};
+
+console.log(canMeasureWater(34,5,6))
+console.log(canMeasureWater(13,11,1))
