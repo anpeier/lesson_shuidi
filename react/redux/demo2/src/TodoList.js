fb568fa5556e6be1ad21ddc2a1f733c12be6977f
anpeier <1490343356@qq.com>
2020-04-19 13:36:18 +0800

    react-redux

--- react/redux/demo2/src/TodoList.js
+++ react/redux/demo2/src/TodoList.js
@@ -1,28 +1,56 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 
-class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div>
-        <input value={this.props.inputVal} />
-        <button>提交</button>
-        <ul>
-          <li>hello</li>
-        </ul>
-      </div>
-    );
-  }
-}
+const TodoList = (props) => {
+  let { inputVal, list, inputChange, addItem, removeItme } = props;
+  return (
+    <div>
+      <input value={inputVal} onChange={inputChange} />
+      <button onClick={addItem}>提交</button>
+      <ul>
+        {list.map((item, index) => {
+          return (
+            <li onClick={removeItme.bind(null, index)} key={item + index}>
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 const stateToProps = (state) => {
   return {
     inputVal: state.inputVal,
+    list: state.list,
   };
 };
 
-export default connect(stateToProps, null)(TodoList);
+const dispathToProps = (dispatch) => {
+  return {
+    inputChange(e) {
+      let action = {
+        type: "change_input",
+        value: e.target.value,
+      };
+      dispatch(action);
+    },
+    addItem() {
+      let action = {
+        type: "add_item",
+      };
+      dispatch(action);
+    },
+    removeItme(index) {
+      console.log(index);
+      let action = {
+        type: "remove_item",
+        index,
+      };
+      dispatch(action);
+    },
+  };
+};
+
+export default connect(stateToProps, dispathToProps)(TodoList);
--- react/redux/demo2/src/store/reducer.js
+++ react/redux/demo2/src/store/reducer.js
@@ -4,5 +4,23 @@ const defaultState = {
 };
 
 export default (state = defaultState, action) => {
+  if (action.type === "change_input") {
+    let newState = JSON.parse(JSON.stringify(state));
+    newState.inputVal = action.value;
+    return newState;
+  }
+  if (action.type === "add_item") {
+    console.log(action);
+    let newState = JSON.parse(JSON.stringify(state));
+    newState.list.push(newState.inputVal);
+    newState.inputVal = "";
+    return newState;
+  }
+  if (action.type === "remove_item") {
+    console.log(action.index);
+    let newState = JSON.parse(JSON.stringify(state));
+    newState.list.splice(action.index, 1);
+    return newState;
+  }
   return state;
 };
