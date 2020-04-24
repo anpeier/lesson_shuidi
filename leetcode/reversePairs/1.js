8c47206acea99fdf19aa8df6fb02c0bc38352361
anpeier <1490343356@qq.com>
2020-04-25 00:16:44 +0800

    数组中的逆序对

--- leetcode/reversePairs/1.js
+++ leetcode/reversePairs/1.js
@@ -4,16 +4,54 @@
 // 示例 1:
 // 输入: [7,5,6,4]
 // 输出: 5
-var reversePairs = function(nums) {
-  if(!nums.length) return 0
-   let count = 0
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i+1; j < nums.length; j++) {
-      if(nums[j]<nums[i]){
-        count++
+
+// var reversePairs = function(nums) {
+//   if(!nums.length) return 0
+//    let count = 0
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i+1; j < nums.length; j++) {
+//       if(nums[j]<nums[i]){
+//         count++
+//       }
+//     }
+//   }
+//   return count
+// };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var reversePairs = function (nums) {
+  // 归并排序
+  let sum = 0;
+  mergeSort(nums);
+  return sum;
+
+  function mergeSort(nums) {
+    if (nums.length < 2) return nums;
+    const mid = parseInt(nums.length / 2);
+    let left = nums.slice(0, mid);
+    let right = nums.slice(mid);
+    return merge(mergeSort(left), mergeSort(right));
+  }
+
+  function merge(left, right) {
+    let res = [];
+    let leftLen = left.length;
+    let rightLen = right.length;
+    let len = leftLen + rightLen;
+    for (let index = 0, i = 0, j = 0; index < len; index++) {
+      if (i >= leftLen) res[index] = right[j++];
+      else if (j >= rightLen) res[index] = left[i++];
+      else if (left[i] <= right[j]) res[index] = left[i++];
+      else {
+        res[index] = right[j++];
+        sum += leftLen - i; //在归并排序中唯一加的一行代码
       }
     }
+    return res;
   }
-  return count
 };
-console.log(reversePairs([7,5,6,4]))
+
+console.log(reversePairs([7, 5, 6, 4]));
