a45b4c93776a0b8204c69359c5f48f5ddf806dea
anpeier <1490343356@qq.com>
2020-04-26 22:01:31 +0800

    hooks

--- react/antd-pro-tags/src/utils/fetch.ts
+++ react/antd-pro-tags/src/utils/fetch.ts
@@ -1,7 +1,7 @@
 // interface + type
 type HttpMethods = 'get' | 'GET' | 'post' | 'POST' | 'DELETE' | 'delete';
 type FuncType = (url: string, method: HttpMethods) => Promise<any>; // 特别的函数 return Promise
-const fetchData: FuncType = (url: string, method: HttpMethods) => {
+const fetchData: FuncType = (url: string) => {
   return new Promise((resolve, reject) => {
     fetch(url)
       .then((res) => res.json())
