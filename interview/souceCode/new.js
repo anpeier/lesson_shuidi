2bc7f17de6c49a2f0759766c6c1b5e752ff02dcb
anpeier <1490343356@qq.com>
2020-05-25 19:01:28 +0800

    .\interview\

--- interview/souceCode/new.js
+++ interview/souceCode/new.js
@@ -1,3 +1,8 @@
+// 创建一个空的简单JavaScript对象（即{}）；
+// 链接该对象（即设置该对象的构造函数）到另一个对象 ；
+// 将步骤1新创建的对象作为this的上下文 ；
+// 如果该函数没有返回对象，则返回this。
+
 function myNew(fn, ...arg) {
   let obj = {};
   Object.setPrototypeOf(obj, fn.prototype);
